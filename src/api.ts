7f24bdcc358b158dbde08aa31c260d9a16ff1cad
johnsmccain <johnsdanlami@gmail.com>
2024-06-21 23:31:49 +0100

    clean up

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-export const getAll = (endpoint) =>  axios.get(`https://guild-audit-leader-board.onrender.com/${endpoint}`);
-export const getById = (endpoint, id) =>  axios.get(`https://guild-audit-leader-board.onrender.com/${endpoint}/${id}`);
+export const getAll = (endpoint:String) =>  axios.get(`https://guild-audit-leader-board.onrender.com/${endpoint}`);
+export const getById = (endpoint:String, id:String) =>  axios.get(`https://guild-audit-leader-board.onrender.com/${endpoint}/${id}`);
