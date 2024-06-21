cbc9cd61685088eda0616c9d73a0d010cf1b3499
johnsmccain <johnsdanlami@gmail.com>
2024-06-21 23:27:40 +0100

    Fix deployment error

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { Card } from '../../components/card/Card'
-import  { getAll } from '../../api'
+import { useEffect, useState } from "react";
+import { Card } from "../../components/card/Card";
+import { getAll } from "../../api";
 export const Home = () => {
-  const [grades, setGrades] = useState([])
-  const fetchData = async () => {
-    const grades = await getAll('grades');
-    const students = await getAll('students');
+	const [grades, setGrades] = useState([]);
+	const fetchData = async () => {
+		const grades = await getAll("grades");
+		const students = await getAll("students");
 
-    setGrades(grades.data)
-    setGrades(students.data)
-  }
-  useEffect(() => {
+		setGrades(grades.data);
+		setGrades(students.data);
+	};
+	useEffect(() => {
+		fetchData();
+	}, []);
 
+	return (
+		<div className="mx-auto max-w-[700px] bg-black bg-opacity-30 rounded-md grid p-5 h-[90%] overflow-hidden  text-amber-50">
+			<div className="grid grid-cols-2 text-2xl my-3">
+				<h2 className="w-[70%] text-center">Profile</h2>
 
-      fetchData()
-  }, [])
-
-
-  return (
-
-    <div className='mx-auto max-w-[700px] bg-black bg-opacity-30 rounded-md grid p-5 h-[90%] overflow-hidden  text-amber-50'>
-        <div className='grid grid-cols-2 text-2xl my-3'>
-            <h2 className='w-[70%] text-center'>Profile</h2>
-
-            <h2 className='text-center'>AVG</h2>
-        </div>
-        <div className=' overflow-hidden overflow-y-scroll no-scrollbar'>
-
-        {grades.map((data, id) => <Card data={data} key={id}/>)}
-        </div>
-    </div>
-  )
-}
+				<h2 className="text-center">AVG</h2>
+			</div>
+			<div className=" overflow-hidden overflow-y-scroll no-scrollbar">
+				{grades.map((data, id) => (
+					<Card data={data} key={id} />
+				))}
+			</div>
+		</div>
+	);
+};
